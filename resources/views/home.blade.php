45be97a3cf581810d66368aa739878db1e3d7c26
unknown <muhammadsandi1919@gmail.com>
2022-07-27 11:35:03 +0700

    Penambahan Template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.main')
+
+@section('content')
+<div class="main-content">
+    <section class="section">
+        <div class="section-header">
+            <h1>Data</h1>
+        </div>
+@endsection
