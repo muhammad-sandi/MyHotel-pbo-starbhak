6a548d7cbb2a642e80774c64a988ef91d64a2ff6
Muhammad Sandi Haikal <muhammadsandi1911@gmail.com>
2022-07-29 10:15:29 +0700

    Membuat Navbar

---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <a class="navbar-brand fw-bold fs-2 ms-3" href="#">MyHotel</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-3">  
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active fw-bold" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fw-bold" href="#">Kamar</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fw-bold" href="#">Fasilitas</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fw-bold" href="#">Sign In</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+  </body>
+</html>
